3bf1b4aeb7d71f8602d4a97986d6a4d8bababda4
Mr.Shi <chnt7305@gmail.com>
2017-03-23 00:17:14 +0800

    update

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 ######################  Defaults  #######################
 
+# install shadowsocks-libev 2.6.0-1 (the source code has been backed up)
 # for debian7.X user please according the follow comments to 
 # enable debian-backports to install systemd-compatibility packages like dh-systemd or init-system-helpers
 # vi /etc/apt/sources.list
@@ -20,20 +21,43 @@ cd shadowsocks-libev
 dpkg-buildpackage -b -us -uc -i
 cd ..
 sudo dpkg -i shadowsocks-libev*.deb
+
+# install shadowsocks-libev 3.x frome soure
+apt-get update
+apt-get install vim git
+
+# configure vim
+cat>/root/.vimrc<<EOF
+set number
+syntax on
+EOF
+
+# install shadowsocks-libev 3.x
+git clone https://github.com/shadowsocks/shadowsocks-libev.git
+cd shadowsocks-libev
+git submodule update --init --recursive
+sh -c 'printf "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/jessie-backports.list'
+apt-get update
+apt-get -t jessie-backports install libmbedtls-dev libsodium-dev
+apt-get install --no-install-recommends gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libudns-dev automake
+./autogen.sh
+./configure --prefix=/usr
+make && make install
+mkdir -p /etc/shadowsocks-libev
+cp ./debian/shadowsocks-libev.init /etc/init.d/shadowsocks-libev		
+cp ./debian/shadowsocks-libev.default /etc/default/shadowsocks-libev		
+cp ./debian/shadowsocks-libev.service /lib/systemd/system/		
+cp ./debian/config.json /etc/shadowsocks-libev/config.json
+chmod +x /etc/init.d/shadowsocks-libev
     
 # install shadowsocksR
 git clone https://github.com/shadowsocksr/shadowsocksr.git
-
-# install serverspeeder 
-wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/serverspeeder/master/serverspeeder-all.sh && bash serverspeeder-all.sh
-# restart serverspeeder
-/serverspeeder/bin/serverSpeeder.sh restart
 
 # install net-speeder
 apt-get install unzip
 apt-get install libnet1-dev
 apt-get install libpcap0.8-dev
-wget https://github.com/snooda/net-speeder/archive/master.zip
+wget https://github.com/chnt7305/net-speeder/archive/master.zip
 unzip master.zip
 # venetX,OpenVZ not Xen,KVM
 sh build.sh -DCOOKED
