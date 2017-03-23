151fd57f731af4e354099fba27aa11f0e722a2d4
Mr.Shi <chnt7305@gmail.com>
2017-03-23 18:20:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,15 +52,102 @@ chmod +x /etc/init.d/shadowsocks-libev
     
 # install shadowsocksR
 git clone https://github.com/shadowsocksr/shadowsocksr.git
+cd shadowsocksr
+bash initcfg.sh
+cd ..
+
+cat>/etc/init.d/shadowsocksr<<EOF
+#!/bin/sh
+# chkconfig: 2345 90 10
+# description: Start or stop the Shadowsocks R server
+#
+### BEGIN INIT INFO
+# Provides: Shadowsocks-R
+# Required-Start: $network $syslog
+# Required-Stop: $network
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Description: Start or stop the Shadowsocks R server
+### END INIT INFO
+
+# Author: Yvonne Lu(Min) <min@utbhost.com>
+
+name=shadowsocksr
+PY=/usr/bin/python
+SS=/root/shadowsocksr/shadowsocks/server.py
+SSPY=server.py
+conf=/root/shadowsocksr/user-config.json
+
+start(){
+    $PY $SS -c $conf -d start
+    RETVAL=$?
+    if [ "$RETVAL" = "0" ]; then
+        echo "$name start success"
+    else
+        echo "$name start failed"
+    fi
+}
+
+stop(){
+    pid=`ps -ef | grep -v grep | grep -v ps | grep -i "${SSPY}" | awk '{print $2}'`
+    if [ ! -z $pid ]; then
+        $PY $SS -c $conf -d stop
+        RETVAL=$?
+        if [ "$RETVAL" = "0" ]; then
+            echo "$name stop success"
+        else
+            echo "$name stop failed"
+        fi
+    else
+        echo "$name is not running"
+        RETVAL=1
+    fi
+}
+
+status(){
+    pid=`ps -ef | grep -v grep | grep -v ps | grep -i "${SSPY}" | awk '{print $2}'`
+    if [ -z $pid ]; then
+        echo "$name is not running"
+        RETVAL=1
+    else
+        echo "$name is running with PID $pid"
+        RETVAL=0
+    fi
+}
+
+case "$1" in
+'start')
+    start
+    ;;
+'stop')
+    stop
+    ;;
+'status')
+    status
+    ;;
+'restart')
+    stop
+    start
+    RETVAL=$?
+    ;;
+*)
+    echo "Usage: $0 { start | stop | restart | status }"
+    RETVAL=1
+    ;;
+esac
+exit $RETVAL
+EOF
+
+chmod 755 /etc/init.d/shadowsocksr ; update-rc.d shadowsocksr defaults ; service shadowsocksr start
 
 # install net-speeder
-apt-get install unzip
-apt-get install libnet1-dev
-apt-get install libpcap0.8-dev
-wget https://github.com/chnt7305/net-speeder/archive/master.zip
-unzip master.zip
+# apt-get install unzip
+# apt-get install libnet1-dev
+# apt-get install libpcap0.8-dev
+# wget https://github.com/chnt7305/net-speeder/archive/master.zip
+# unzip master.zip
 # venetX,OpenVZ not Xen,KVM
-sh build.sh -DCOOKED
+# sh build.sh -DCOOKED
 
 # Configure iptables
 apt-get install iptables
