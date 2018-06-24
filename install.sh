99e8fae2b451ae7d1b9f05ebe84edfe4e0ba19fe
Mr.Shi <9529884+chnt7305@users.noreply.github.com>
2018-06-24 17:01:42 +0800

    remove shadowsocks-libev install

--- install.sh
+++ install.sh
@@ -7,21 +7,6 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 ######################  Defaults  #######################
 
-# install shadowsocks-libev 2.6.0-1 (the source code has been backed up)
-# for debian7.X user please according the follow comments to 
-# enable debian-backports to install systemd-compatibility packages like dh-systemd or init-system-helpers
-# vi /etc/apt/sources.list
-# deb http://ftp.debian.org/debian wheezy-backports main
-# apt-get update
-# apt-get install git
-# apt-get install --no-install-recommends build-essential autoconf libtool libssl-dev \
-#    gawk debhelper dh-systemd init-system-helpers pkg-config asciidoc xmlto apg libpcre3-dev
-# git clone https://github.com/shadowsocks/shadowsocks-libev.git
-# cd shadowsocks-libev
-# dpkg-buildpackage -b -us -uc -i
-# cd ..
-# dpkg -i shadowsocks-libev*.deb
-
 apt-get update
 apt-get install vim git screen
 
@@ -30,24 +15,6 @@ cat>/root/.vimrc<<EOF
 set number
 syntax on
 EOF
-
-# install shadowsocks-libev 3.x frome soure
-git clone https://github.com/shadowsocks/shadowsocks-libev.git
-cd shadowsocks-libev
-git submodule update --init --recursive
-sh -c 'printf "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/jessie-backports.list'
-apt-get update
-apt-get -t jessie-backports install libmbedtls-dev libsodium-dev
-apt-get install --no-install-recommends gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libudns-dev automake
-./autogen.sh
-./configure --prefix=/usr
-make && make install
-mkdir -p /etc/shadowsocks-libev
-cp ./debian/shadowsocks-libev.init /etc/init.d/shadowsocks-libev		
-cp ./debian/shadowsocks-libev.default /etc/default/shadowsocks-libev		
-cp ./debian/shadowsocks-libev.service /lib/systemd/system/		
-cp ./debian/config.json /etc/shadowsocks-libev/config.json
-chmod +x /etc/init.d/shadowsocks-libev
     
 # install shadowsocksR
 git clone https://github.com/chnt7305/shadowsocksr.git
@@ -138,16 +105,6 @@ exit $RETVAL
 EOF
 
 chmod 755 /etc/init.d/shadowsocksr 
-
-# install net-speeder
-# apt-get install unzip
-# apt-get install libnet1-dev
-# apt-get install libpcap0.8-dev
-# wget https://github.com/chnt7305/net-speeder/archive/master.zip
-# unzip master.zip
-# venetX,OpenVZ not Xen,KVM
-# sh build.sh -DCOOKED
-# nohup /root/net-speeder-master/net_speeder venet0 "ip" >/dev/null 2>&1 &
 
 # Configure iptables
 apt-get install iptables
