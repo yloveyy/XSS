872458e289a8970af15c82f2a2e91914d57be092
Mr.Shi <9529884+chnt7305@users.noreply.github.com>
2019-02-26 19:56:23 +0800

    Update ss-libev-install.sh

--- ss-libev-install.sh
+++ ss-libev-install.sh
@@ -12,7 +12,7 @@ export PATH
 ######################  Defaults  #######################
 
 apt-get update
-apt-get install git haveged -y
+apt-get install git -y
 
 git clone https://github.com/shadowsocks/shadowsocks-libev.git
 cd shadowsocks-libev
@@ -21,7 +21,7 @@ git submodule update --init --recursive
 apt-get install --no-install-recommends gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libc-ares-dev automake -y
 
 # Build libsodium
-export LIBSODIUM_VER=1.0.16
+export LIBSODIUM_VER=1.0.17
 wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz
 tar xvf libsodium-$LIBSODIUM_VER.tar.gz
 pushd libsodium-$LIBSODIUM_VER
@@ -31,7 +31,7 @@ popd
 ldconfig
 
 #Build mbedtls
-export MBEDTLS_VER=2.13.0
+export MBEDTLS_VER=2.16.0
 wget https://tls.mbed.org/download/mbedtls-$MBEDTLS_VER-gpl.tgz
 tar xvf mbedtls-$MBEDTLS_VER-gpl.tgz
 pushd mbedtls-$MBEDTLS_VER
@@ -51,9 +51,6 @@ cp ./debian/shadowsocks-libev.default /etc/default/shadowsocks-libev
 cp ./debian/shadowsocks-libev.service /lib/systemd/system/		
 cp ./debian/config.json /etc/shadowsocks-libev/config.json
 chmod +x /etc/init.d/shadowsocks-libev
-
-# make sure haveged configured to start on boot
-update-rc.d haveged defaults
 
 echo "Shadowsocks-libev install completed"
 
